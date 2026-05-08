1dc61ca373a9ffd7bd61343035293d03a8c95940
Dream <gracehoong@gmail.com>
2026-05-09 00:57:09 +0800

    fix

---
+++ assets/site.js
@@ -0,0 +1,4 @@
+document.addEventListener('click', function(e){
+  const a = e.target.closest('a[href^="#"]');
+  if(a){ const el=document.querySelector(a.getAttribute('href')); if(el){ e.preventDefault(); el.scrollIntoView({behavior:'smooth'}); } }
+});
